a8f0d84dc35fa7493bc29fa254c0faaa9145e3af
joom122796 <113173627+joom122796@users.noreply.github.com>
2022-10-06 13:57:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-import keypad.keypad as keypad
+import keypad
 
-def SecSyst():
-  Alarm = False
+Alarm = False
+
+def AlarmSys():
+  Alarm = True
+  if Alarm == True:
+    print('*Alarm noise*')
+    print('Sent notification to home owners device ')
+    print('System lockdown')
+    quit()
+  else:
+    print('System idle...')
+
+def SecSys():
   SecuritySystem = True
   while SecuritySystem == True:
     movementGround = int(input('Is there movement on the ground?\nEnter 1 for Yes and enter 2 for No\nEnter: '))
-    movementUpper = int(input('Is there movement on the second floor?\nEnter 1 for Yes and enter 2 for No\nEnter: '))
     if movementGround == 1:
-      tries = 0
-      while tries < 5:
-        fhome = int(input('Enter keypad to disable alarm system: '))
-        if fhome != keypad:
-          tries = tries + 1
-          total = 5 - tries
-          print(f'Incorrect keypad entry. You have {total} tries left')
-        else: 
-          print('Family is home, disabling security system.')
-          SecuritySystem = False
+      fhome = int(input('Enter passcode to disable alarm system: '))
+      if fhome == keypad.password:
+        SecuritySystem = False
+        print('Family is home, disabling security system.')
       else:
         print('Movement on the ground detected: enabling intrusion detection system')
-        Alarm = True
-        if Alarm == True:
-          print('*Alarm noise*')
-        print('Sent notification to home owners device ')
+        AlarmSys()
     else:
-        print('System idle... ')
-
+      print('System idle... ')
+    movementUpper = int(input('Is there movement on the second floor?\nEnter 1 for Yes and enter 2 for No\nEnter: '))
     if movementUpper == 1:
       print('Movement on the second floor detected: enabling intrusion detection system')
-      Alarm = True
-      if Alarm == True:
-        print('*Alarm noise*')
-      print('Sent notification to home owners device ')
+      AlarmSys()
     else:
       print('System idle... ')
   else:
@@ -39,6 +37,6 @@ def SecSyst():
 
 fleave = int(input('Is the family leaving the house?\nEnter 1 for Yes and enter 2 for No \nEnter:  '))
 if fleave == 1:
-  SecSyst()
+  SecSys()
 else:
   print('Security system remains disabled.\nSystem idle...')
