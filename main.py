e321fb7eff3f3cd45de7910bae048bd063e8492d
joom122796 <113173627+joom122796@users.noreply.github.com>
2022-10-07 10:58:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-import secsys
-from secsys import SecSys as Security
-import keypad
-import leave
-
-pw = int(input('Is there a passcode set to lock/unlock the home security system\nEnter 1 for Yes and enter 2 for No \nEnter:  '))
-
-if pw == 2:
-  pw2 = int(input('Enter a four digit passcode to set as your security code: '))
-  pwconf = int(input('Re-enter to confirm your four digit passcode: '))
-  while pwconf != pw2:
-    pwconf = int(input('Invalid entry!\nRe-enter to confirm your four digit passcode: '))
-  else:
-    print('Your passcode has updated.')
-    keypad.password = pwconf
-    leave.leaving()
-else:
-  leave.leaving()
+import setup
+setup.setup()
