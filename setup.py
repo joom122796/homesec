bdbff0792ba6800cb5af4ab57068a2782878c71b
joom122796 <113173627+joom122796@users.noreply.github.com>
2022-10-07 11:18:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,29 @@ import leave
 from leave import leaving as leaving
 
 def setup():
-  pw = int(input('Is there a passcode set to lock/unlock the home security system\nEnter 1 for Yes and enter 2 for No \nEnter:  '))
-  if pw == 2:
-    pw2 = int(input('Enter a four digit passcode to set as your security code: '))
-    pwconf = int(input('Re-enter to confirm your four digit passcode: '))
+  if keypad.password == '0000':
+    pw2 = int(input('It seems to be your first time using the home security system.\nEnter a four digit passcode to set as your security code: '))
+    pwconf = int(input('Re-enter the four digit passcode to confirm your security code: '))
     while pwconf != pw2:
-      pwconf = int(input('Invalid entry!\nRe-enter to confirm your four digit passcode: '))
+      pwconf = int(input('Invalid entry!\nRe-enter to confirm your four digit security code: '))
     else:
-      print('Your passcode has updated.')
-      keypad.password = pwconf
+        print('Your passcode has updated.')
+        keypad.password = pwconf
+        leaving()
+  else: 
+    pw = int(input('There currently is a passcode set to lock/unlock the home security system. Would you like to reset your password? \nEnter 1 for Yes and enter 2 for No \nEnter:  '))
+    if pw == 1:
+      reset = int(input('Enter the current passcode to reset your security code: '))
+      while reset != keypad.password:
+        reset = int(input('Invalid password!\nEnter four digit passcode to reset your security code: '))
+      else:
+        pw2 = int(input('Enter a four digit passcode to set as your security code: '))
+        pwconf = int(input('Re-enter to confirm your four digit security code: '))
+        while pwconf != pw2:
+          pwconf = int(input('Invalid entry!\nRe-enter to confirm your four digit security code: '))
+        else:
+          print('Your passcode has updated.')
+          keypad.password = pwconf
+          leaving()
+    else:
       leaving()
-  else:
-    leaving()
