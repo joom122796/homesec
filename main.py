d7e3bd2e24e68d0cc04d166a7966461e304d2258
joom122796 <113173627+joom122796@users.noreply.github.com>
2022-10-07 10:15:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import secsys
-from secsys import SecSys
+from secsys import SecSys as Security
 
 Alarm = False
 
 fleave = int(input('Is the family leaving the house?\nEnter 1 for Yes and enter 2 for No \nEnter:  '))
 if fleave == 1:
-  SecSys()
+  Security()
 else:
   print('Security system remains disabled.\nSystem idle...')
