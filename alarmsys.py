e3a1f02a2c178e7284e1f0b99867bfc7ac5acc31
joom122796 <113173627+joom122796@users.noreply.github.com>
2022-10-07 10:04:40 +0100

    Create alarmsys.py

---
+++ alarmsys.py
@@ -0,0 +1,9 @@
+def AlarmSys():
+  Alarm = True
+  if Alarm == True:
+    print('*Alarm noise*')
+    print('Sent notification to home owners device ')
+    print('System lockdown')
+    quit()
+  else:
+    print('System idle...')
